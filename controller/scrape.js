3bd05be026bd521ef1ed8ad1cea1fb35f97be8c8
Owner-PC\hen <btnysci@yahoo.com>
2017-03-06 00:25:14 -0800

    Implemented ORM method

--- controller/scrape.js
+++ controller/scrape.js
@@ -1,3 +1,57 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
+module.exports = function(response, scraper, displayScrape) {
+
+	request('http://www.gamespot.com/', function(err, res, html){
+
+		if(err)
+		{
+			console.log(err)
+			// reject(err);
+		}
+		else
+		{
+
+			var scrapedStuff = [];
+			console.log("GOT RESPONSE FROM GAMESPOT");
+			var $ = cheerio.load(html);
+			// var articleCounter = 1;
+
+			$('article a').each(function(i, element){
+
+				// var scrapedStuff = {};
+
+				// scrapedStuff.articleID = articleCounter;
+				scrapedStuff.push({
+					title: $(this).attr('data-event-title'),
+					link:  $(this).attr('href'),
+					img: $(this).children('figure').children('div.media-img').children('img').attr('src'),
+					description: $(this).children('div.media-body').children('p.media-deck').text()
+				})
+				// scrapedStuff.title = $(this).attr('data-event-title');
+				// scrapedStuff.link = $(this).attr('href');
+				// scrapedStuff.img = $(this).children('figure').children('div.media-img').children('img').attr('src');
+				// scrapedStuff.description = $(this).children('div.media-body').children('p.media-deck').text();
+
+				console.log("SCRAPED RESULTS", scrapedStuff);
+				// articleCounter++;
+
+				// var scrapedArticles = new Article(scrapedStuff);
+
+				// scrapedArticles.save(function(err, doc){
+				// 	if(err){
+				// 		console.log(err)
+				// 	}else{
+				// 		console.log("NEW METHOD DOCS", doc);
+
+				// 		// response.render('home', {data: doc, finishScrape: true})
+				// 	}
+				// })
+
+			});
+			scraper(response, scrapedStuff, displayScrape);
+
+		}
+	});
+}
