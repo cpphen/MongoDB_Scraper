5510f5b6b98d1c7199ef4c89164855635bb80415
Owner-PC\hen <btnysci@yahoo.com>
2017-03-07 11:58:43 -0800

    CHANGESs

--- models/Article.js
+++ models/Article.js
@@ -20,11 +20,7 @@ var ArticleSchema = new Schema({
 	},
 	img: {
 		type: String
-	},
-	comment: [{
-		type: Schema.Types.ObjectId,
-		ref: 'Comment'
-	}]
+	}
 });
 
 var Article = mongoose.model("Article", ArticleSchema);
